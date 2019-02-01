e4841c406a8125a9696069b1edee6b3ea0b750eb
danielnewell <46284599+danielnewell@users.noreply.github.com>
2019-02-01 11:59:18 -0500

    Added Perms loader
    added the permissions loader because permissions seem a little important for something called PermissionsJS

--- library/stores/json.js
+++ library/stores/json.js
@@ -13,9 +13,7 @@ async function fetchUsers (userFile) {
   return new Promise((resolve, reject) => {
     try {
       var users = JSON.parse(require('fs').readdirSync(`./${userFile}`))
-      if(!users) {
-        users = require('../defaults/users.json')
-      }
+      if(!users) users = require('../defaults/users.json')
       return resolve({ users })
     } catch (err) {
       if(err.message.indexOf('ENOENT') > -1) {
@@ -31,14 +29,28 @@ async function fetchGroups(groupFile) {
   return new Promise((resolve, reject) => {
     try {
       var groups = JSON.parse(require('fs').readdirSync(`./${groupFile}`))
-      if(!groups) {
-        groups = require('../defaults/groups.json')
-      }
+      if(!groups) groups = require('../defaults/groups.json')
       return resolve({ groups })
     } catch (err) {
       if(err.message.indexOf('ENOENT') > -1) {
         var groups = require('../defaults/groups.json')
         return resolve({ groups })
+      }
+      return reject(err)
+    }
+  })
+}
+async function fetchPerms(permFile) {
+  return new Promise((resolve, reject) => {
+    try {
+      var perms = JSON.prse(require('fs').readdirSync(`./${permFile}))
+      if(!perms) perms = require('../defaults/perms.json')
+      return resolve({ perms })
+    }
+    catch (err) {
+      if(err.message.indexOf('ENOENT') > -1) {
+        var perms = require('../defaults/perms.json')
+        return resolve({ perms })
       }
       return reject(err)
     }
